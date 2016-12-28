b46e4696d9d32ab5065763f9f769497201152a11
vineet.tyagi <vineet.tyagi@digital.hmrc.gov.uk>
2016-12-28 17:22:30 +0000

    IHT-4321 Add tests

---
+++ test/iht/views/registration/kickout/IhtKickoutTemplateViewTest.scala
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package iht.views.registration.kickout
+
+import iht.utils.ApplicationKickOutHelper
+import iht.views.ViewTestHelper
+import play.api.i18n.Messages
+import play.twirl.api.Html
+import iht.views.html.registration.kickout.iht_kickout_template
+
+/**
+  * Created by vineet on 15/11/16.
+  */
+class IhtKickoutTemplateViewTest extends ViewTestHelper{
+
+  val title = "selected title"
+  val summaryMessage = "kickout summary message"
+  val returnLinkUrl = iht.controllers.registration.deceased.routes.DeceasedDateOfDeathController.onPageLoad
+
+  def ihtKickOutTemplateView() = {
+    implicit val request = createFakeRequest()
+
+    val view = iht_kickout_template(title,
+                        summaryMessage,
+                        returnLinkUrl,
+                        false)(Html("")).toString()
+    asDocument(view)
+  }
+
+  "IhtKickoutApplication View" must {
+    "have the correct title and summary message" in {
+      val view = ihtKickOutTemplateView()
+
+      titleShouldBeCorrect(view.toString, title)
+      //browserTitleShouldBeCorrect(view.toString, title)
+      messagesShouldBePresent(view.toString, summaryMessage)
+    }
+
+    "have 'Next steps' heading" in {
+      val view = ihtKickOutTemplateView
+      val headers = view.getElementsByTag("h2")
+
+      headers.first.text() shouldBe Messages("iht.nextSteps")
+    }
+
+    "have details are correct button " in {
+      val view = ihtKickOutTemplateView
+
+      val detailsAreCorrectButton = view.getElementById("finish")
+      detailsAreCorrectButton.attr("value") shouldBe Messages("site.button.details.correct.exitToGovK")
+    }
+
+    "have return link with correct text" in {
+      val view = ihtKickOutTemplateView
+
+      val detailsAreCorrectButton = view.getElementById("return-button")
+      detailsAreCorrectButton.attr("href") shouldBe returnLinkUrl.url
+      detailsAreCorrectButton.text shouldBe Messages("iht.registration.kickout.message.returnToLast")
+    }
+  }
+
+}
