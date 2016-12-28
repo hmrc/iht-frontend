cf9e44faa539b29106b707f96009ff37cae9415d
vineet.tyagi <vineet.tyagi@digital.hmrc.gov.uk>
2016-12-28 15:13:07 +0000

    IHT-4321 Add tests

---
+++ test/iht/views/registration/CompletedRegistrationViewTest.scala
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package iht.views.registration
+
+import iht.views.ViewTestHelper
+import play.api.i18n.Messages
+import iht.utils._
+import iht.views.html.registration.completed_registration
+
+class CompletedRegistrationViewTest extends ViewTestHelper{
+
+  val ihtRef = "A1A1A1"
+
+  "CompletedRegistrationView" must {
+
+    "contain the correct title and browser title" in {
+      implicit val request = createFakeRequest()
+      val view = completed_registration(ihtRef).toString
+
+      titleShouldBeCorrect(view, Messages("iht.registration.complete"))
+      browserTitleShouldBeCorrect(view, Messages("iht.registration.complete"))
+    }
+
+    "contain the correct guidance" in {
+      implicit val request = createFakeRequest()
+      val view = completed_registration(ihtRef).toString
+
+      messagesShouldBePresent(view, Messages("page.iht.registration.completedRegistration.ref.title"))
+      messagesShouldBePresent(view, Messages("page.iht.registration.completedRegistration.ref.text"))
+      messagesShouldBePresent(view, Messages("iht.nextSteps"))
+      messagesShouldBePresent(view, Messages("page.iht.registration.completedRegistration.p1"))
+      messagesShouldBePresent(view, Messages("page.iht.registration.completedRegistration.p1"))
+      messagesShouldBePresent(view, Messages("page.iht.registration.completedRegistration.p1"))
+    }
+
+    "contain correct formatted reference number" in {
+      implicit val request = createFakeRequest()
+      val view = completed_registration(ihtRef).toString
+
+      messagesShouldBePresent(view, formattedIHTReference(ihtRef))
+    }
+
+   "contain button with correct text and target as Estate report page" in {
+
+      implicit val request = createFakeRequest()
+      val view = completed_registration(ihtRef).toString
+      val doc = asDocument(view)
+
+      val link = doc.getElementById("go-to-inheritance-tax-report")
+      link.text shouldBe Messages("site.button.continue.iht.app.page")
+      link.attr("href") shouldBe iht.controllers.home.routes.IhtHomeController.onPageLoad.url
+
+    }
+
+  }
+}
