a206000b8b1d7be6fc61f6abde6ce5516a7fdbc7
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2017-01-03 11:20:35 +0000

    IHT-4173 Refactor for PDF controller and xmlfop

--- app/iht/controllers/application/pdf/PDFController.scala
+++ app/iht/controllers/application/pdf/PDFController.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 HM Revenue & Customs
+ * Copyright 2017 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,8 +20,6 @@ import iht.config.FrontendAuthConnector
 import iht.connector.{CachingConnector, IhtConnector}
 import iht.controllers.application.ApplicationController
 import iht.controllers.auth.IhtActions
-import iht.models.RegistrationDetails
-import iht.models.application.ApplicationDetails
 import iht.utils.pdf._
 import iht.utils.{CommonHelper, DeclarationHelper}
 import models.des.iht_return.IHTReturn
@@ -29,8 +27,7 @@ import play.api.Logger
 import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
 import uk.gov.hmrc.play.http.HeaderCarrier
 
-import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future}
+import scala.concurrent.Future
 
 /**
   * Created by dbeer on 14/08/15.
@@ -53,24 +50,14 @@ trait PDFController extends ApplicationController with IhtActions {
   def onPDFSummary = authorisedForIht {
     implicit user => implicit request => {
       Logger.info("Generating Summary PDF")
-      val regDetails:RegistrationDetails = cachingConnector.getExistingRegistrationDetails
-      val applicationDetails: Option[ApplicationDetails] = Await.result(ihtConnector.getApplication(CommonHelper.getNino(user),
-        CommonHelper.getOrExceptionNoIHTRef(regDetails.ihtReference),
-        regDetails.acknowledgmentReference),
-        Duration.Inf)
-      val declarationType: String = DeclarationHelper.getDeclarationType(applicationDetails.fold(throw new RuntimeException)(identity))
-      val declaration = declarationType match {
-        case "" => false
-        case _ => {
-          Logger.info("Declaration Type = " + declarationType)
-          true
-        }
+      withApplicationDetails { regDetails => applicationDetails =>
+        val pdfByteArray = xmlFoToPDF.generateSummaryPDF(
+          regDetails,
+          applicationDetails,
+          DeclarationHelper.getDeclarationType(applicationDetails)
+        )
+        Future.successful(Ok(pdfByteArray).withHeaders(("Content-type", "application/pdf")))
       }
-
-      val kickout = CommonHelper.getOrExceptionNoApplication(applicationDetails).kickoutReason.isEmpty
-
-      val pdfByteArray = xmlFoToPDF.generateSummaryPDF(regDetails, applicationDetails, declaration, declarationType, kickout)
-      Future.successful(Ok(pdfByteArray).withHeaders(("Content-type", "application/pdf")))
     }
   }
 
@@ -78,23 +65,20 @@ trait PDFController extends ApplicationController with IhtActions {
     implicit user => implicit request => {
       Logger.info("Generating Clearance PDF")
       val nino = CommonHelper.getNino(user)
-      ihtConnector.getCaseDetails(nino, ihtReference).map(registrationDetails =>
+      ihtConnector.getCaseDetails(nino, ihtReference).flatMap(registrationDetails =>
         getSubmittedApplicationDetails(nino,
           CommonHelper.getOrExceptionNoIHTRef(registrationDetails.ihtReference),
-          registrationDetails.updatedReturnId) match {
-          case Some(ihtReturn) => {
-              val pdfByteArray = xmlFoToPDF.createClearancePDF(registrationDetails, CommonHelper.getOrException(
-                ihtReturn.declaration, "No declaration found").declarationDate.getOrElse(
-                throw new RuntimeException("Declaration Date not available")))
-
+          registrationDetails.updatedReturnId) map {
+          case Some(ihtReturn) =>
+            val pdfByteArray = xmlFoToPDF.createClearancePDF(registrationDetails, CommonHelper.getOrException(
+              ihtReturn.declaration, "No declaration found").declarationDate.getOrElse(
+              throw new RuntimeException("Declaration Date not available")))
             Ok(pdfByteArray).withHeaders(("Content-type", "application/pdf"))
-          }
-          case _ => {
+          case _ =>
             Logger.warn("There has been a problem retrieving the details for the Application PDF. Redirecting" +
               " to internalServerError")
             InternalServerError("There has been a problem retrieving the details for the " +
               "Application PDF")
-          }
         }
       )
     }
@@ -104,44 +88,33 @@ trait PDFController extends ApplicationController with IhtActions {
     implicit user => implicit request => {
       val nino = CommonHelper.getNino(user)
 
-      ihtConnector.getCaseDetails(nino, ihtReference).map(regDetails =>
-        getSubmittedApplicationDetails(nino, ihtReference, regDetails.updatedReturnId) match {
-          case Some(ihtReturn) => {
+      ihtConnector.getCaseDetails(nino, ihtReference).flatMap(regDetails =>
+        getSubmittedApplicationDetails(nino, ihtReference, regDetails.updatedReturnId) map {
+          case Some(ihtReturn) =>
             val pdfByteArray = xmlFoToPDF.createApplicationReturnPDF(regDetails, ihtReturn)
             Ok(pdfByteArray).withHeaders(("Content-type", "application/pdf"))
-          }
-          case _ => {
+          case _ =>
             Logger.warn("There has been a problem retrieving the details for the Application PDF. Redirecting" +
               " to internalServerError")
             InternalServerError("There has been a problem retrieving the details for the " +
               "Application PDF")
-          }
         }
       )
     }
   }
 
   /**
-    *
     * Retrieves IHTReturn for given ihtRef and returnId
-    *
-    * @param nino
-    * @param ihtReference
-    * @param returnId
-    * @return
     */
   private def getSubmittedApplicationDetails(nino: String, ihtReference: String, returnId: String)
-                                            (implicit headerCarrier: HeaderCarrier): Option[IHTReturn] = {
-    Await.result(ihtConnector.getSubmittedApplicationDetails(nino, ihtReference, returnId),
-      Duration.Inf) match {
-      case Some(ihtReturn) => {
+                                            (implicit headerCarrier: HeaderCarrier): Future[Option[IHTReturn]] = {
+    ihtConnector.getSubmittedApplicationDetails(nino, ihtReference, returnId) map { optionIHTReturn =>
+      if(optionIHTReturn.isDefined) {
         Logger.info("IhtReturn details have been successfully retrieved ")
-        Some(ihtReturn)
-      }
-      case _ => {
+      } else {
         Logger.warn("IhtReturn details not found")
-        None
       }
+      optionIHTReturn
     }
   }
 }
