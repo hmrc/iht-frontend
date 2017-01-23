254ef152e71cfb112a405a88d73d279e817f2cd9
David Beer <david.beer@digital.hmrc.gov.uk>
2017-01-23 13:29:18 +0000

    Upgrade of libraries and abt version for play 2.5.8

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -8,7 +8,7 @@ object FrontendBuild extends Build with MicroService {
 }
 
 private object AppDependencies {
-  import play.PlayImport._
+  import play.sbt.PlayImport._
   import play.core.PlayVersion
 
   private val httpCachingClientVersion = "5.6.0"
@@ -18,19 +18,19 @@ private object AppDependencies {
 val compile = Seq(
   ws, cache,
   "uk.gov.hmrc" %% "play-health" % "1.1.0",
-  "uk.gov.hmrc" %% "play-ui" % "4.17.2",
-  "uk.gov.hmrc" %% "govuk-template" % "4.0.0",
-  "uk.gov.hmrc" %% "url-builder" % "1.0.0",
+  "uk.gov.hmrc" %% "play-ui" % "5.2.0",
+  "uk.gov.hmrc" %% "govuk-template" % "5.0.0",
+  "uk.gov.hmrc" %% "url-builder" % "2.0.0",
   "uk.gov.hmrc" %% "http-caching-client" % httpCachingClientVersion,
-  "uk.gov.hmrc" %% "frontend-bootstrap" % "6.7.0",
-  "uk.gov.hmrc" %% "play-partials" % "4.6.0",
-  "uk.gov.hmrc" %% "play-config" % "2.1.0",
+  "uk.gov.hmrc" %% "frontend-bootstrap" % "7.10.0",
+  "uk.gov.hmrc" %% "play-partials" % "5.2.0",
+  "uk.gov.hmrc" %% "play-config" % "3.0.0",
   "uk.gov.hmrc" %% "play-json-logger" % "2.1.1",
   "uk.gov.hmrc" %% "passcode-verification" % "3.5.0",
-  "uk.gov.hmrc" %% "play-authorised-frontend" % "5.8.0",
-  "uk.gov.hmrc" %% "http-verbs" % "5.0.0",
+  "uk.gov.hmrc" %% "play-authorised-frontend" % "6.2.0",
+  "uk.gov.hmrc" %% "http-verbs" % "6.2.0",
   "uk.gov.hmrc" %% "play-auditing" % "1.9.0",
-  "uk.gov.hmrc" %% "domain" % "3.7.0",
+  "uk.gov.hmrc" %% "domain" % "4.0.0",
   "com.github.fge" % "json-schema-validator" % jsonSchemaValidatorVersion,
   "uk.gov.hmrc" %% "play-graphite" % "2.0.0",
   "org.apache.xmlgraphics" % "fop" % "2.1",
