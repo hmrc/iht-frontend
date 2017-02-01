0ee5065c324a10c33999443283ac1e2d1f9df9ca
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2017-02-01 17:29:33 +0000

    IHT-4124 Unit tests

--- test/iht/views/registration/executor/OthersApplyingForProbateViewTest.scala
+++ test/iht/views/registration/executor/OthersApplyingForProbateViewTest.scala
@@ -16,37 +16,48 @@
 
 package iht.views.registration.executor
 
-import iht.forms.registration.ApplicantForms.applyingForProbateForm
-import iht.views.registration.RegistrationPageBehaviour
 import iht.forms.registration.CoExecutorForms.othersApplyingForProbateForm
-import iht.models.{ApplicantDetails, DeceasedDateOfDeath}
-import iht.views.html.registration.applicant.applying_for_probate
 import iht.views.html.registration.executor.others_applying_for_probate
+import iht.views.registration.YesNoQuestionViewBehaviour
+import org.jsoup.nodes.Document
 import play.api.data.Form
 import play.api.i18n.Messages
 import play.api.mvc.Call
 import play.twirl.api.HtmlFormat.Appendable
 
-class OthersApplyingForProbateViewTest extends RegistrationPageBehaviour[Option[Boolean]] {
+class OthersApplyingForProbateViewTest extends YesNoQuestionViewBehaviour[Option[Boolean]] {
+
+  override def guidanceParagraphs = Set(Messages("page.iht.registration.others-applying-for-probate.description"))
 
   override def pageTitle = Messages("page.iht.registration.others-applying-for-probate.sectionTitle")
+
   override def browserTitle = Messages("page.iht.registration.others-applying-for-probate.browserTitle")
 
-  override def form:Form[Option[Boolean]] = othersApplyingForProbateForm
-  override def formToView:Form[Option[Boolean]] => Appendable = form => others_applying_for_probate(form, Call("", ""))
+  override def form: Form[Option[Boolean]] = othersApplyingForProbateForm
+
+  override def formToView: Form[Option[Boolean]] => Appendable = form => others_applying_for_probate(form, Call("", ""))
 
   "Others Applying for Probate View" must {
+    behave like yesNoQuestion
+  }
 
-    behave like registrationPage()
+  "Others Applying for Probate View in Edit Mode" must {
+    "have a continue and cancel link with correct targets" in {
+      val view: Document = {
+        implicit val request = createFakeRequest()
+        val view = others_applying_for_probate(othersApplyingForProbateForm,
+          Call("GET", "submit"), Some(Call("GET", "cancel"))).toString
+        asDocument(view)
+      }
 
-    "have a fieldset with the Id 'answer'" in {
-      val view = others_applying_for_probate(othersApplyingForProbateForm, Call("", ""))(createFakeRequest()).toString
-      asDocument(view).getElementsByTag("fieldset").first.id shouldBe "answer"
-    }
+      val continueLink = view.getElementById("continue-button")
+      continueLink.attr("value") shouldBe Messages("iht.continue")
 
-    "show the correct guidance" in {
-      messagesShouldBePresent(view,
-        Messages("page.iht.registration.others-applying-for-probate.description"))
+      val ook = view.getElementsByTag("form").attr("action") shouldBe "submit"
+
+      val cancelLink = view.getElementById("cancel-button")
+      cancelLink.attr("href") shouldBe "cancel"
+      cancelLink.text() shouldBe Messages("site.link.cancel")
     }
   }
 }
