6836169c36f9f8f059b3509433227c8bd2a8cfd9
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2017-02-13 15:44:21 +0000

    IHT-4436 Tidy

--- test/iht/views/application/YesNoQuestionViewBehaviour.scala
+++ test/iht/views/application/YesNoQuestionViewBehaviour.scala
@@ -16,27 +16,10 @@
 
 package iht.views.application
 
-import iht.testhelpers.CommonBuilder
-import iht.views.ViewTestHelper
-import org.jsoup.nodes.Document
 import play.api.i18n.Messages
-import play.api.mvc.{AnyContentAsEmpty, Call}
-import play.api.test.FakeRequest
+import play.api.mvc.Call
 
 trait YesNoQuestionViewBehaviour[A] extends ApplicationPageBehaviour[A] {
-
-  // Legacy defs - to be removed
-  def yesNoQuestionText: String = "???"
-  def returnLinkId: String = "return-button"
-  def returnLinkText: String = ""
-  def returnLinkTargetUrl: Call = Call("","")
-  def fixture() = new {
-    implicit val request: FakeRequest[AnyContentAsEmpty.type] = createFakeRequest()
-    val view: String = ""
-    val doc: Document = new Document("")
-  }
-  //
-
   def pageTitle: String
 
   def browserTitle: String
@@ -61,91 +44,16 @@ trait YesNoQuestionViewBehaviour[A] extends ApplicationPageBehaviour[A] {
     }
 
     "show the Save/Continue button with the correct target" in {
-      //val continueButton = doc.getElementsByClass("button")
       val continueButton = doc.getElementById("save-continue")
       doc.getElementsByTag("form").attr("action") shouldBe formTarget.url
     }
   }
 
-//  def yesNoQuestionWithLegend(questionLegend: => String) = {
-//    yesNoQuestion()
-//
-//    "show the correct question text" in {
-//      doc.getElementById("yes-no-question-legend").text shouldBe questionLegend
-//    }
-//  }
+  def yesNoQuestionWithLegend(questionLegend: => String) = {
+    yesNoQuestion()
 
-//  /**
-//    * Assumes that the Call for the continue button has been set up as CommonBuilder.DefaultCall1.
-//    * Assumes that the Call for the cancel link has been set up as CommonBuilder.DefaultCall2.
-//    */
-//  def yesNoQuestionWithCancelLink() = {
-//    yesNoQuestion()
-//
-//    "show the Cancel link with the correct target" in {
-//      val continueButton = doc.getElementById("continue-button")
-//      continueButton.`val` shouldBe Messages("iht.continue")
-//      doc.getElementsByTag("form").attr("action") shouldBe CommonBuilder.DefaultCall1.url
-//
-//      val cancelLink = doc.getElementById("cancel-button")
-//      cancelLink.attr("href") shouldBe CommonBuilder.DefaultCall2.url
-//      cancelLink.text() shouldBe Messages("site.link.cancel")
-//    }
-//  }
+    "show the correct question text" in {
+      doc.getElementById("yes-no-question-legend").text shouldBe questionLegend
+    }
+  }
 }
-
-
-// Original content of this file:-
-//
-//  def pageTitle: String
-//  def browserTitle: String
-//  def guidanceParagraphs: Set[String]
-//  def yesNoQuestionText: String
-//  def returnLinkId: String = "return-button"
-//  def returnLinkText: String
-//  def returnLinkTargetUrl: Call
-//
-//  def fixture() = new {
-//    implicit val request: FakeRequest[AnyContentAsEmpty.type] = createFakeRequest()
-//    val view: String = ""
-//    val doc: Document = new Document("")
-//  }
-//
-//  def yesNoQuestion() = {
-//    "have the correct title" in {
-//      val f = fixture()
-//      val doc = asDocument(f.view)
-//      val headers = doc.getElementsByTag("h1")
-//      headers.size shouldBe 1
-//      headers.first.text() shouldBe pageTitle
-//    }
-//
-//    "have the correct browser title" in {
-//      val f = fixture()
-//      browserTitleShouldBeCorrect(f.view, browserTitle)
-//    }
-//
-//    "show the correct guidance paragraphs" in {
-//      val f = fixture()
-//      for (paragraph <- guidanceParagraphs) messagesShouldBePresent(f.view, paragraph)
-//    }
-//
-//    "show the correct yes/no question text" in {
-//      val f = fixture()
-//      messagesShouldBePresent(f.view, yesNoQuestionText)
-//    }
-//
-//    "show the Save and continue button" in {
-//      val f = fixture()
-//      val saveAndContinueButton = f.doc.getElementById("save-continue")
-//      saveAndContinueButton.text() shouldBe Messages("iht.saveAndContinue")
-//    }
-//
-//    "show the correct return link with text" in {
-//      val f = fixture()
-//      val returnLink = f.doc.getElementById(returnLinkId)
-//      returnLink.attr("href") shouldBe returnLinkTargetUrl.url
-//      returnLink.text() shouldBe returnLinkText
-//    }
-//  }
-//}
