e7d2639ae5ee1c3ffce9ec8d960edbcaef88e77b
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2017-02-14 14:28:40 +0000

    IHT-4478 Add tests for each failure page

---
+++ test/iht/views/iv/failurepages/InsufficientEvidenceViewTest.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package iht.views.iv.failurepages
+
+import iht.views.html.iv.failurepages.{insufficient_evidence, precondition_failed}
+import iht.views.{ExitComponent, GenericNonSubmittablePageBehaviour}
+import play.api.i18n.Messages
+import play.api.mvc.Call
+
+class InsufficientEvidenceViewTest extends GenericNonSubmittablePageBehaviour {
+  def guidanceParagraphs = Set(
+    Messages("iht.iv.unableToContinue"),
+    Messages("iht.iv.tryAgainLater")
+  )
+
+  def pageTitle = Messages("error.problem")
+
+  def browserTitle = Messages("error.problem")
+
+  def view: String = insufficient_evidence("IVURL").toString
+
+  override def exitComponent = Some(
+    ExitComponent(
+      Call("GET", "IVURL"),
+      Messages("iht.iv.tryAgain")
+    )
+  )
+
+  "Insufficient Evidence View" must {
+    behave like nonSubmittablePage()
+  }
+}
---
+++ test/iht/views/iv/failurepages/UserAbortedViewTest.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package iht.views.iv.failurepages
+
+import iht.views.html.iv.failurepages.user_aborted
+import iht.views.{ExitComponent, GenericNonSubmittablePageBehaviour}
+import play.api.i18n.Messages
+import play.api.mvc.Call
+
+class UserAbortedViewTest extends GenericNonSubmittablePageBehaviour {
+  def guidanceParagraphs = Set(
+    Messages("page.iht.iv.failure.userAborted.p1")
+  )
+
+  def pageTitle = Messages("page.iht.iv.failure.userAborted.title")
+
+  def browserTitle = Messages("page.iht.iv.failure.userAborted.title")
+
+  def view: String = user_aborted("IVURL").toString
+
+  override def exitComponent = Some(
+    ExitComponent(
+      Call("GET", "IVURL"),
+      Messages("iht.iv.tryAgain")
+    )
+  )
+
+  "User Aborted View" must {
+    behave like nonSubmittablePage()
+  }
+}
