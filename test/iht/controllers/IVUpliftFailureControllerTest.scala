0abdd1c87dc8522dfbaac2919c6292b45a21715e
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2017-02-14 16:48:34 +0000

    IHT-4478 Refactor

--- test/iht/controllers/IVUpliftFailureControllerTest.scala
+++ test/iht/controllers/IVUpliftFailureControllerTest.scala
@@ -23,6 +23,7 @@ import iht.models.enums.IdentityVerificationResult.IdentityVerificationResult
 import org.mockito.Matchers._
 import org.mockito.Mockito._
 import play.api.i18n.Messages
+import play.api.mvc.Result
 import play.api.test.Helpers.{OK, _}
 import uk.gov.hmrc.play.http.HeaderCarrier
 
@@ -31,75 +32,50 @@ import scala.concurrent.Future
 class IVUpliftFailureControllerTest extends ApplicationControllerTest {
   implicit val hc = new HeaderCarrier
   val mockIdentityVerificationConnector = mock[IdentityVerificationConnector]
+
   def controller = new IVUpliftFailureController {
     override val identityVerificationConnector = mockIdentityVerificationConnector
   }
 
-  def ivFailureApplication(failureName: String, ivResult: IdentityVerificationResult, titleMessagesKey:String) = {
+  def ivFailure(showFailure: Option[String] => Future[Result], failureName: String, ivResult: IdentityVerificationResult, titleMessagesKey: String) = {
     s"Go to the $failureName page when the appropriate IV verification result is received" in {
       when(mockIdentityVerificationConnector.identityVerificationResponse(any())(any()))
         .thenReturn(Future.successful(ivResult))
-      val result = controller.showNotAuthorisedApplication(Some(""))(createFakeRequest())
+      val result = showFailure(Some(""))
       status(result) should be(OK)
       contentAsString(result) should include(Messages(titleMessagesKey))
     }
   }
 
-  def ivFailureRegistration(failureName: String, ivResult: IdentityVerificationResult, titleMessagesKey:String) = {
-    s"Go to the $failureName page when the appropriate IV verification result is received" in {
-      when(mockIdentityVerificationConnector.identityVerificationResponse(any())(any()))
-        .thenReturn(Future.successful(ivResult))
-      val result = controller.showNotAuthorisedRegistration(Some(""))(createFakeRequest())
-      status(result) should be(OK)
-      contentAsString(result) should include(Messages(titleMessagesKey))
-    }
-  }
-
-  "showNotAuthorisedApplication" must {
+  def iVUpliftFailureBehaviour(showFailure: Option[String] => Future[Result]) = {
     "go to 2fa failure page if no journey id" in {
-      val result = controller.showNotAuthorisedApplication(None)(createFakeRequest())
+      val result = showFailure(None)
       status(result) should be(OK)
       contentAsString(result) should include(Messages("page.iht.iv.failure.2fa.title"))
     }
 
-    behave like ivFailureApplication("failed matching", IdentityVerificationResult.FailedMatching, "page.iht.iv.failure.failedMatching.title")
+    behave like ivFailure(showFailure, "failed matching", IdentityVerificationResult.FailedMatching, "page.iht.iv.failure.failedMatching.title")
 
-    behave like ivFailureApplication("incomplete", IdentityVerificationResult.Incomplete, "error.problem")
+    behave like ivFailure(showFailure, "incomplete", IdentityVerificationResult.Incomplete, "error.problem")
 
-    behave like ivFailureApplication("insufficient evidence", IdentityVerificationResult.InsufficientEvidence, "error.problem")
+    behave like ivFailure(showFailure, "insufficient evidence", IdentityVerificationResult.InsufficientEvidence, "error.problem")
 
-    behave like ivFailureApplication("locked out", IdentityVerificationResult.LockedOut, "page.iht.iv.failure.lockedOut.title")
+    behave like ivFailure(showFailure, "locked out", IdentityVerificationResult.LockedOut, "page.iht.iv.failure.lockedOut.title")
 
-    behave like ivFailureApplication("precondition failed", IdentityVerificationResult.PreconditionFailed, "page.iht.iv.failure.preconditionFailed.title")
+    behave like ivFailure(showFailure, "precondition failed", IdentityVerificationResult.PreconditionFailed, "page.iht.iv.failure.preconditionFailed.title")
 
-    behave like ivFailureApplication("technical issue", IdentityVerificationResult.TechnicalIssue, "page.iht.iv.failure.technicalIssue.title")
+    behave like ivFailure(showFailure, "technical issue", IdentityVerificationResult.TechnicalIssue, "page.iht.iv.failure.technicalIssue.title")
 
-    behave like ivFailureApplication("timeout", IdentityVerificationResult.Timeout, "page.iht.iv.failure.timeout.title")
+    behave like ivFailure(showFailure, "timeout", IdentityVerificationResult.Timeout, "page.iht.iv.failure.timeout.title")
 
-    behave like ivFailureApplication("user aborted", IdentityVerificationResult.UserAborted, "page.iht.iv.failure.userAborted.title")
+    behave like ivFailure(showFailure, "user aborted", IdentityVerificationResult.UserAborted, "page.iht.iv.failure.userAborted.title")
   }
 
   "showNotAuthorisedRegistration" must {
-    "go to 2fa failure page if no journey id" in {
-      val result = controller.showNotAuthorisedRegistration(None)(createFakeRequest())
-      status(result) should be(OK)
-      contentAsString(result) should include(Messages("page.iht.iv.failure.2fa.title"))
-    }
+    behave like iVUpliftFailureBehaviour(x=>controller.showNotAuthorisedRegistration(x)(createFakeRequest()))
+  }
 
-    behave like ivFailureRegistration("failed matching", IdentityVerificationResult.FailedMatching, "page.iht.iv.failure.failedMatching.title")
-
-    behave like ivFailureRegistration("incomplete", IdentityVerificationResult.Incomplete, "error.problem")
-
-    behave like ivFailureRegistration("insufficient evidence", IdentityVerificationResult.InsufficientEvidence, "error.problem")
-
-    behave like ivFailureRegistration("locked out", IdentityVerificationResult.LockedOut, "page.iht.iv.failure.lockedOut.title")
-
-    behave like ivFailureRegistration("precondition failed", IdentityVerificationResult.PreconditionFailed, "page.iht.iv.failure.preconditionFailed.title")
-
-    behave like ivFailureRegistration("technical issue", IdentityVerificationResult.TechnicalIssue, "page.iht.iv.failure.technicalIssue.title")
-
-    behave like ivFailureRegistration("timeout", IdentityVerificationResult.Timeout, "page.iht.iv.failure.timeout.title")
-
-    behave like ivFailureRegistration("user aborted", IdentityVerificationResult.UserAborted, "page.iht.iv.failure.userAborted.title")
+  "showNotAuthorisedApplication" must {
+    behave like iVUpliftFailureBehaviour(x=>controller.showNotAuthorisedApplication(x)(createFakeRequest()))
   }
 }
