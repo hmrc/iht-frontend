db321ef3c6b4ecf23e7dbc50ddbc3c654bc51308
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2017-03-20 15:25:59 +0000

    IHT-LANG-UNIT-TEST-FIX Fix unit tests

--- test/iht/testhelpers/MessagesTidierTest.scala
+++ test/iht/testhelpers/MessagesTidierTest.scala
@@ -28,7 +28,7 @@ class MessagesTidierTest extends UnitSpec with FakeIhtApp {
 
   val runTestsThatUseFileSystem = false
   val runTestsThatSortErrorMessages = false
-  val runTestsThatSortIhtMessages = false
+  val runTestsThatSortIhtMessages = true
   val runTestsThatReplaceMessageKeys = false
   val runTestsThatGenerateAllErrorMessageKeysForReplace = false
 
@@ -693,7 +693,7 @@ class MessagesTidierTest extends UnitSpec with FakeIhtApp {
           val sortedErrorMessages = MessagesTidier.sortErrormessagesApi(messagesAsTuples)
           mockedMessagesTidier.writeToFile("/home/grant/Desktop/waa1.txt", sortedErrorMessages)
 
-          val inputFilePath = getResourceAsFilePath("messages")
+          val inputFilePath = getResourceAsFilePath("messages.en")
 
           val result1: Seq[(String, String)] = MessagesTidier.getNonErrorMessagesExclCommentedKeys(inputFilePath)
           mockedMessagesTidier.writeToFile("/home/grant/Desktop/waa2.txt", result1)
