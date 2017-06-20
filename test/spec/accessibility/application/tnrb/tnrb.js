9a979cdece50861b16fef1794fd7281dc4c095a5
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2017-06-20 17:18:22 +0100

    IHT-4709 TNRB

--- test/spec/accessibility/application/tnrb/tnrb.js
+++ test/spec/accessibility/application/tnrb/tnrb.js
@@ -30,98 +30,8 @@ describe('TNRB, accessibility : ', function() {
       });
     });
 
-    function fillDeceasedEverWidowed(done, driver){
-        driver.get('http://localhost:9070/inheritance-tax/estate-report/deceased-ever-widowed')
-        driver.findElement(By.css('#yes-label')).click();
-        actionHelper.submitPageHelper(done, driver);
-    }
-
-    function fillDateOfDeath(done, driver){
-        driver.get(Browser.baseUrl + '/estate-report/date-of-death')
-        driver.executeScript("document.querySelector('[name=\"dateOfPreDeceased.day\"]').setAttribute('value', '1')");
-        driver.executeScript("document.querySelector('[name=\"dateOfPreDeceased.month\"]').setAttribute('value', '12')");
-        driver.executeScript("document.querySelector('[name=\"dateOfPreDeceased.year\"]').setAttribute('value', '2001')");
-        actionHelper.submitPageHelper(done, driver);
-    }
-
-    function fillNameOfSpouse(done, driver){
-        driver.get(Browser.baseUrl + '/estate-report/name-of-spouse-or-civil-partner')
-        driver.executeScript("document.querySelector('[name=\"firstName\"]').setAttribute('value', 'Jo')");
-        driver.executeScript("document.querySelector('[name=\"lastName\"]').setAttribute('value', 'Higgins')");
-        actionHelper.submitPageHelper(done, driver);
-    }
-
-    function fillDateOfMarriage(done, driver){
-        driver.get(Browser.baseUrl + '/estate-report/marriage-or-civil-partnership-date')
-        driver.executeScript("document.querySelector('[name=\"dateOfMarriage.day\"]').setAttribute('value', '1')");
-        driver.executeScript("document.querySelector('[name=\"dateOfMarriage.month\"]').setAttribute('value', '12')");
-        driver.executeScript("document.querySelector('[name=\"dateOfMarriage.year\"]').setAttribute('value', '1990')");
-        actionHelper.submitPageHelper(done, driver);
-    }
-
-    function fillPermanentHome(done, driver){
-        driver.get(Browser.baseUrl + '/estate-report/permanent-home-location')
-        driver.findElement(By.css('#yes-label')).click();
-        actionHelper.submitPageHelper(done, driver);
-    }
-
-    function fillFullyExempt(done, driver){
-        driver.get(Browser.baseUrl + '/estate-report/fully-exempt-estate')
-        driver.findElement(By.css('#yes-label')).click();
-        actionHelper.submitPageHelper(done, driver);
-    }
-
-    function fillJointAssets(done, driver){
-        driver.get(Browser.baseUrl + '/estate-report/joint-assets-in-estate')
-        driver.findElement(By.css('#yes-label')).click();
-        actionHelper.submitPageHelper(done, driver);
-    }
-
-    function fillReliefClaimed(done, driver){
-        driver.get(Browser.baseUrl + '/estate-report/any-relief-claimed')
-        driver.findElement(By.css('#no-label')).click();
-        actionHelper.submitPageHelper(done, driver);
-    }
-
-    function fillBenefitFromTrust(done, driver){
-        driver.get(Browser.baseUrl + '/estate-report/any-trusts-in-estate')
-        driver.findElement(By.css('#no-label')).click();
-        actionHelper.submitPageHelper(done, driver);
-    }
-
-    function fillNonExemptGifts(done, driver){
-        driver.get(Browser.baseUrl + '/estate-report/any-non-exempt-gifts')
-        driver.findElement(By.css('#no-label')).click();
-        actionHelper.submitPageHelper(done, driver);
-    }
-
-    function fillTypeGifts(done, driver){
-        driver.get(Browser.baseUrl + '/estate-report/gift-types-given-away')
-        driver.findElement(By.css('#no-label')).click();
-        actionHelper.submitPageHelper(done, driver);
-    }
-
-    function fillRequired(done, driver){
-        fillDeceasedEverWidowed(done, driver)
-        fillDateOfDeath(done, driver)
-    }
-
-    function fillTnrb(done, driver){
-        fillRequired(done, driver)
-        fillNameOfSpouse(done, driver)
-        fillDateOfMarriage(done, driver)
-        fillPermanentHome(done, driver)
-        fillFullyExempt(done, driver)
-        fillJointAssets(done, driver)
-        fillReliefClaimed(done, driver)
-        fillBenefitFromTrust(done, driver)
-        fillNonExemptGifts(done, driver)
-        fillTypeGifts(done, driver)
-    }
-
-
     it('tnrb overview, filled', function (done) {
-        fillTnrb(done, driver)
+        actionHelper.populateApplicationData(driver, 'TNRBAllFilled');
 
         behaves.actsAsBasicPage(done, driver, {
             url: Browser.baseUrl + '/estate-report/increasing-the-threshold',
@@ -130,7 +40,7 @@ describe('TNRB, accessibility : ', function() {
     });
 
     it('threshold increased', function (done) {
-        fillTnrb(done, driver)
+        actionHelper.populateApplicationData(driver, 'TNRBAllFilled');
 
         behaves.actsAsBasicPage(done, driver, {
             url: Browser.baseUrl + '/estate-report/threshold-increased',
@@ -154,7 +64,7 @@ describe('TNRB, accessibility : ', function() {
     });
 
     it('date of death', function (done) {
-        fillDeceasedEverWidowed(done, driver)
+        actionHelper.populateApplicationData(driver, 'TNRBDeceasedEverWidowedFilled');
 
         behaves.actsAsStandardForm(done, driver, {
             url: Browser.baseUrl + '/estate-report/date-of-death',
@@ -164,7 +74,7 @@ describe('TNRB, accessibility : ', function() {
     });
 
     it('name of spouse', function (done) {
-        fillRequired(done, driver)
+        actionHelper.populateApplicationData(driver, 'TNRBDeceasedEverWidowedDateOfDeathFilled');
 
         behaves.actsAsStandardForm(done, driver, {
             url: Browser.baseUrl + '/estate-report/name-of-spouse-or-civil-partner',
@@ -174,7 +84,7 @@ describe('TNRB, accessibility : ', function() {
     });
 
     it('date of marriage', function (done) {
-        fillRequired(done, driver)
+        actionHelper.populateApplicationData(driver, 'TNRBDeceasedEverWidowedDateOfDeathFilled');
 
         behaves.actsAsStandardForm(done, driver, {
             url: Browser.baseUrl + '/estate-report/marriage-or-civil-partnership-date',
@@ -184,7 +94,7 @@ describe('TNRB, accessibility : ', function() {
     });
 
     it('permanent home', function (done) {
-        fillRequired(done, driver)
+        actionHelper.populateApplicationData(driver, 'TNRBDeceasedEverWidowedDateOfDeathFilled');
 
         behaves.actsAsStandardForm(done, driver, {
             url: Browser.baseUrl + '/estate-report/permanent-home-location',
@@ -194,7 +104,7 @@ describe('TNRB, accessibility : ', function() {
     });
 
     it('fully exempt estate', function (done) {
-        fillRequired(done, driver)
+        actionHelper.populateApplicationData(driver, 'TNRBDeceasedEverWidowedDateOfDeathFilled');
 
         behaves.actsAsStandardForm(done, driver, {
             url: Browser.baseUrl + '/estate-report/fully-exempt-estate',
@@ -204,7 +114,7 @@ describe('TNRB, accessibility : ', function() {
     });
 
     it('joint assets in estate', function (done) {
-        fillRequired(done, driver)
+        actionHelper.populateApplicationData(driver, 'TNRBDeceasedEverWidowedDateOfDeathFilled');
 
         behaves.actsAsStandardForm(done, driver, {
             url: Browser.baseUrl + '/estate-report/joint-assets-in-estate',
@@ -214,7 +124,7 @@ describe('TNRB, accessibility : ', function() {
     });
 
     it('any relief claimed', function (done) {
-        fillRequired(done, driver)
+        actionHelper.populateApplicationData(driver, 'TNRBDeceasedEverWidowedDateOfDeathFilled');
 
         behaves.actsAsStandardForm(done, driver, {
             url: Browser.baseUrl + '/estate-report/any-relief-claimed',
@@ -224,7 +134,7 @@ describe('TNRB, accessibility : ', function() {
     });
 
     it('trust benefit', function (done) {
-        fillRequired(done, driver)
+        actionHelper.populateApplicationData(driver, 'TNRBDeceasedEverWidowedDateOfDeathFilled');
 
         behaves.actsAsStandardForm(done, driver, {
             url: Browser.baseUrl + '/estate-report/any-trusts-in-estate',
@@ -234,7 +144,7 @@ describe('TNRB, accessibility : ', function() {
     });
 
     it('non-exempt gifts', function (done) {
-        fillRequired(done, driver)
+        actionHelper.populateApplicationData(driver, 'TNRBDeceasedEverWidowedDateOfDeathFilled');
 
         behaves.actsAsStandardForm(done, driver, {
             url: Browser.baseUrl + '/estate-report/any-non-exempt-gifts',
@@ -244,7 +154,7 @@ describe('TNRB, accessibility : ', function() {
     });
 
     it('gifts with reservation of benefit', function (done) {
-        fillRequired(done, driver)
+        actionHelper.populateApplicationData(driver, 'TNRBDeceasedEverWidowedDateOfDeathFilled');
 
         behaves.actsAsStandardForm(done, driver, {
             url: Browser.baseUrl + '/estate-report/gift-types-given-away',
