a44e2761992b051b0106f03e110458c4fda9b983
Adam Liptrot <adam.liptrot@opencastsoftware.com>
2017-06-20 11:39:03 +0100

    Code tidy

--- test/spec/accessibility/registration/registration.js
+++ test/spec/accessibility/registration/registration.js
@@ -1,15 +1,14 @@
-var selenium = require('selenium-webdriver'),
-     
-    AxeBuilder = require('axe-webdriverjs'); 
-var By = selenium.By,
-    until = selenium.until; 
+var selenium = require('selenium-webdriver');
+var AxeBuilder = require('axe-webdriverjs'); 
+var By = selenium.By;
+var until = selenium.until; 
 var colors = require('colors'); 
-var TestReporter = require('../../../spec-helpers/reporter.js'); 
-var Browser = require('../../../spec-helpers/browser.js');
-var accessibilityhelper = require('../../../spec-helpers/check-accessibility-helper.js'); 
-var loginhelper = require('../../../spec-helpers/login-helper.js'); 
-var actionHelper = require('../../../spec-helpers/action-helper.js'); 
-var behaves = require('../../../spec-helpers/behaviour.js');
+var TestReporter = require('../../../spec-helpers/reporter'); 
+var Browser = require('../../../spec-helpers/browser');
+var accessibilityhelper = require('../../../spec-helpers/check-accessibility-helper'); 
+var loginhelper = require('../../../spec-helpers/login-helper'); 
+var actionHelper = require('../../../spec-helpers/action-helper'); 
+var behaves = require('../../../spec-helpers/behaviour');
 var Reporter = new TestReporter();  
 
 
@@ -19,204 +18,202 @@ jasmine.getEnv().clearReporters(); 
 jasmine.getEnv().addReporter(Reporter.reporter);   
 
 fdescribe('Registration accessibility : ', function() { 
-            var driver;  
-            beforeEach(function(done) { 
-                driver = Browser.startBrowser();
-                loginhelper.authenticate(done, driver, 'reg') 
-            });  
+    var driver;  
+    beforeEach(function(done) { 
+        driver = Browser.startBrowser();
+        loginhelper.authenticate(done, driver, 'reg') 
+    });  
 
-            // Close website after each test is run (so it is opened fresh each time) 
-
-            afterEach(function(done) { 
-                driver.quit().then(function() { 
-                    done(); 
-                }); 
-            });   
+    afterEach(function(done) { 
+        driver.quit().then(function() { 
+            done(); 
+        }); 
+    });   
 
 
 
 
-            it('registration checklist', function(done) { 
-                behaves.actsAsBasicPage(done, driver, {
-                 url: Browser.baseUrl + '/registration/registration-checklist',
-                 pageTitle: "Before you start registration"
-                })
-            });  
+    it('registration checklist', function(done) { 
+        behaves.actsAsBasicPage(done, driver, {
+         url: Browser.baseUrl + '/registration/registration-checklist',
+         pageTitle: "Before you start registration"
+        })
+    });  
 
-            it('when did the deceased die', function(done) { 
-                behaves.actsAsStandardForm(done, driver, {
-                  url: Browser.baseUrl + '/registration/date-of-death',
-                  pageTitle: "Date of death",
-                  button: '#continue-button'
-                })
-            });  
+    it('when did the deceased die', function(done) { 
+        behaves.actsAsStandardForm(done, driver, {
+          url: Browser.baseUrl + '/registration/date-of-death',
+          pageTitle: "Date of death",
+          button: '#continue-button'
+        })
+    });  
 
-            it('deceased permanent home', function(done) { 
-                actionHelper.populateRegistrationData(driver, 'DateOfDeath');
+    it('deceased permanent home', function(done) { 
+        actionHelper.populateRegistrationData(driver, 'DateOfDeath');
 
-                behaves.actsAsStandardForm(done, driver, {
-                  url: Browser.baseUrl + '/registration/permanent-home-location',
-                  pageTitle: "Permanent home",
-                  button: '#continue-button'
-                })
-            });  
+        behaves.actsAsStandardForm(done, driver, {
+          url: Browser.baseUrl + '/registration/permanent-home-location',
+          pageTitle: "Permanent home",
+          button: '#continue-button'
+        })
+    });  
 
-              it('deceased details', function(done) { 
-                actionHelper.populateRegistrationData(driver, 'PermanentHomeLocation');
+      it('deceased details', function(done) { 
+        actionHelper.populateRegistrationData(driver, 'PermanentHomeLocation');
 
-                behaves.actsAsStandardForm(done, driver, {
-                  url: Browser.baseUrl + '/registration/deceaseds-details',
-                  pageTitle: "About the person who has died",
-                  button: '#continue-button'
-                })
-              });  
+        behaves.actsAsStandardForm(done, driver, {
+          url: Browser.baseUrl + '/registration/deceaseds-details',
+          pageTitle: "About the person who has died",
+          button: '#continue-button'
+        })
+      });  
 
-              it('deceased contact address', function(done) { 
-                actionHelper.populateRegistrationData(driver, 'DeceasedsDetails');
+      it('deceased contact address', function(done) { 
+        actionHelper.populateRegistrationData(driver, 'DeceasedsDetails');
 
-                behaves.actsAsStandardForm(done, driver, {
-                  url: Browser.baseUrl + '/registration/location-of-contact-address',
-                  pageTitle: "Contact address",
-                  button: '#continue-button'
-                })
-              });  
+        behaves.actsAsStandardForm(done, driver, {
+          url: Browser.baseUrl + '/registration/location-of-contact-address',
+          pageTitle: "Contact address",
+          button: '#continue-button'
+        })
+      });  
 
-              it('deceased contact address UK', function(done) { 
-                actionHelper.populateRegistrationData(driver, 'DeceasedLastAddressUK');
+      it('deceased contact address UK', function(done) { 
+        actionHelper.populateRegistrationData(driver, 'DeceasedLastAddressUK');
 
-                behaves.actsAsStandardForm(done, driver, {
-                  url: Browser.baseUrl + '/registration/uk-contact-address',
-                  pageTitle: "Contact address",
-                  button: '#continue-button'
-                })
-              });  
+        behaves.actsAsStandardForm(done, driver, {
+          url: Browser.baseUrl + '/registration/uk-contact-address',
+          pageTitle: "Contact address",
+          button: '#continue-button'
+        })
+      });  
 
-              it('deceased contact address outside UK', function(done) { 
-                actionHelper.populateRegistrationData(driver, 'DeceasedLastAddressOutsideUK');
+      it('deceased contact address outside UK', function(done) { 
+        actionHelper.populateRegistrationData(driver, 'DeceasedLastAddressOutsideUK');
 
-                behaves.actsAsStandardForm(done, driver, {
-                  url: Browser.baseUrl + '/registration/contact-address',
-                  pageTitle: "Contact address",
-                  button: '#continue-button'
-                })
-              });  
+        behaves.actsAsStandardForm(done, driver, {
+          url: Browser.baseUrl + '/registration/contact-address',
+          pageTitle: "Contact address",
+          button: '#continue-button'
+        })
+      });  
 
-              it('applying for probate', function(done) { 
-                actionHelper.populateRegistrationData(driver, 'UKContactAddress');
+      it('applying for probate', function(done) { 
+        actionHelper.populateRegistrationData(driver, 'UKContactAddress');
 
-                behaves.actsAsStandardForm(done, driver, {
-                  url: Browser.baseUrl + '/registration/applying-for-probate',
-                  pageTitle: "Apply for probate",
-                  button: '#continue-button'
-                })
-              });  
+        behaves.actsAsStandardForm(done, driver, {
+          url: Browser.baseUrl + '/registration/applying-for-probate',
+          pageTitle: "Apply for probate",
+          button: '#continue-button'
+        })
+      });  
 
-              it('applying for probate location', function(done) { 
-                actionHelper.populateRegistrationData(driver, 'ApplyingForProbate');
+      it('applying for probate location', function(done) { 
+        actionHelper.populateRegistrationData(driver, 'ApplyingForProbate');
 
-                behaves.actsAsStandardForm(done, driver, {
-                  url: Browser.baseUrl + '/registration/probate-location',
-                  pageTitle: "Probate location",
-                  button: '#continue-button'
-                })
-              });  
+        behaves.actsAsStandardForm(done, driver, {
+          url: Browser.baseUrl + '/registration/probate-location',
+          pageTitle: "Probate location",
+          button: '#continue-button'
+        })
+      });  
 
-              it('your contact details', function(done) { 
-                actionHelper.populateRegistrationData(driver, 'ProbateLocation');
+      it('your contact details', function(done) { 
+        actionHelper.populateRegistrationData(driver, 'ProbateLocation');
 
-                behaves.actsAsStandardForm(done, driver, {
-                  url: Browser.baseUrl + '/registration/your-contact-details',
-                  pageTitle: "Your contact details",
-                  button: '#continue-button'
-                })
-              });  
+        behaves.actsAsStandardForm(done, driver, {
+          url: Browser.baseUrl + '/registration/your-contact-details',
+          pageTitle: "Your contact details",
+          button: '#continue-button'
+        })
+      });  
 
-              it('your address in the UK', function(done) { 
-                actionHelper.populateRegistrationData(driver, 'ApplicantsContact');
+      it('your address in the UK', function(done) { 
+        actionHelper.populateRegistrationData(driver, 'ApplicantsContact');
 
-                behaves.actsAsStandardForm(done, driver, {
-                  url: Browser.baseUrl + '/registration/your-uk-address',
-                  pageTitle: "Your address",
-                  button: '#continue-button'
-                })
-              })  
+        behaves.actsAsStandardForm(done, driver, {
+          url: Browser.baseUrl + '/registration/your-uk-address',
+          pageTitle: "Your address",
+          button: '#continue-button'
+        })
+      })  
 
-              it('your address outside the UK', function(done) { 
-                actionHelper.populateRegistrationData(driver, 'YourOutsideUKAddress');
+      it('your address outside the UK', function(done) { 
+        actionHelper.populateRegistrationData(driver, 'YourOutsideUKAddress');
 
-                behaves.actsAsStandardForm(done, driver, {
-                  url: Browser.baseUrl + '/registration/your-address',
-                  pageTitle: "Your address",
-                  button: '#continue-button'
-                })
-              })  
+        behaves.actsAsStandardForm(done, driver, {
+          url: Browser.baseUrl + '/registration/your-address',
+          pageTitle: "Your address",
+          button: '#continue-button'
+        })
+      })  
 
-              it('any other applicants', function(done) { 
-                actionHelper.populateRegistrationData(driver, 'YourUKAddress');
+      it('any other applicants', function(done) { 
+        actionHelper.populateRegistrationData(driver, 'YourUKAddress');
 
-                behaves.actsAsStandardForm(done, driver, {
-                  url: Browser.baseUrl + '/registration/any-other-applicants',
-                  pageTitle: "Other probate applicants",
-                  button: '#continue-button'
-                })
-              })  
+        behaves.actsAsStandardForm(done, driver, {
+          url: Browser.baseUrl + '/registration/any-other-applicants',
+          pageTitle: "Other probate applicants",
+          button: '#continue-button'
+        })
+      })  
 
-              it('other applicant details', function(done) { 
-                actionHelper.populateRegistrationData(driver, 'AnyOtherApplicants');
+      it('other applicant details', function(done) { 
+        actionHelper.populateRegistrationData(driver, 'AnyOtherApplicants');
 
-                behaves.actsAsStandardForm(done, driver, {
-                  url: Browser.baseUrl + '/registration/applicants-details',
-                  pageTitle: "Other person’s details",
-                  button: '#continue-button'
-                })
-              })  
+        behaves.actsAsStandardForm(done, driver, {
+          url: Browser.baseUrl + '/registration/applicants-details',
+          pageTitle: "Other person’s details",
+          button: '#continue-button'
+        })
+      })  
 
-              it('other applicant address UK', function(done) { 
-                actionHelper.populateRegistrationData(driver, 'ApplicantsDetailsIndicatingAUKAddress');
+      it('other applicant address UK', function(done) { 
+        actionHelper.populateRegistrationData(driver, 'ApplicantsDetailsIndicatingAUKAddress');
 
-                behaves.actsAsStandardForm(done, driver, {
-                  url: Browser.baseUrl + '/registration/applicants-uk-address/1',
-                  pageTitle: "Other person’s address",
-                  button: '#continue-button'
-                })
-              })  
+        behaves.actsAsStandardForm(done, driver, {
+          url: Browser.baseUrl + '/registration/applicants-uk-address/1',
+          pageTitle: "Other person’s address",
+          button: '#continue-button'
+        })
+      })  
 
-              it('other applicant address outside UK', function(done) { 
-                actionHelper.populateRegistrationData(driver, 'ApplicantsDetails');
+      it('other applicant address outside UK', function(done) { 
+        actionHelper.populateRegistrationData(driver, 'ApplicantsDetails');
 
-                behaves.actsAsStandardForm(done, driver, {
-                    url: Browser.baseUrl + '/registration/applicants-uk-address/1',
-                    pageTitle: "Other person’s address",
-                    button: '#continue-button'
-                })
-              })  
+        behaves.actsAsStandardForm(done, driver, {
+            url: Browser.baseUrl + '/registration/applicants-uk-address/1',
+            pageTitle: "Other person’s address",
+            button: '#continue-button'
+        })
+      })  
 
-              it('other people applying for probate', function(done) { 
-                actionHelper.populateRegistrationData(driver, 'ApplicantsDetails');
+      it('other people applying for probate', function(done) { 
+        actionHelper.populateRegistrationData(driver, 'ApplicantsDetails');
 
-                behaves.actsAsStandardForm(done, driver, {
-                  url: Browser.baseUrl + '/registration/other-probate-applicants',
-                  pageTitle: "Other applicants",
-                  button: '#continue-button'
+        behaves.actsAsStandardForm(done, driver, {
+          url: Browser.baseUrl + '/registration/other-probate-applicants',
+          pageTitle: "Other applicants",
+          button: '#continue-button'
 
-                })
-              })  
+        })
+      })  
 
-              it('delete other applicant', function(done) { 
-                actionHelper.populateRegistrationData(driver, 'CompleteWith2CoExecutors');
+      it('delete other applicant', function(done) { 
+        actionHelper.populateRegistrationData(driver, 'CompleteWith2CoExecutors');
 
-                behaves.actsAsBasicPage(done, driver, {
-                   url: Browser.baseUrl + '/registration/delete-applicant/1',
-                   pageTitle: "Delete applicant"
-                });
-              })  
+        behaves.actsAsBasicPage(done, driver, {
+           url: Browser.baseUrl + '/registration/delete-applicant/1',
+           pageTitle: "Delete applicant"
+        });
+      })  
 
-              it('check your answers', function(done) { 
-                actionHelper.populateRegistrationData(driver, 'CompleteWith2CoExecutors');
+      it('check your answers', function(done) { 
+        actionHelper.populateRegistrationData(driver, 'CompleteWith2CoExecutors');
 
-                behaves.actsAsBasicPage(done, driver, {
-                    url: Browser.baseUrl + '/registration/check-your-answers',
-                    pageTitle: "Check your answers"
-                });
-              })   
-          });         
+        behaves.actsAsBasicPage(done, driver, {
+            url: Browser.baseUrl + '/registration/check-your-answers',
+            pageTitle: "Check your answers"
+        });
+      })   
+});         
