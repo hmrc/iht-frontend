d764cdae248458e200c7f91dcbf63424d29085d3
vineet.tyagi <vineet.tyagi@digital.hmrc.gov.uk>
2017-07-27 01:20:58 +0100

    IHT-4938 Add test for Article and Sidebar

--- test/iht/views/ihtHelpers/standard/ArticleTest.scala
+++ test/iht/views/ihtHelpers/standard/ArticleTest.scala
@@ -18,35 +18,31 @@ package iht.views.ihtHelpers.standard
 
 import iht.FakeIhtApp
 import iht.views.HtmlSpec
-import iht.views.html.ihtHelpers.standard.{sidebar, input_radio_group_with_hints}
-import play.api.data.Form
-import play.api.data.Forms._
-import play.api.i18n.Messages.Implicits._
+import iht.views.html.ihtHelpers.standard.{article, sidebar}
 import play.twirl.api.Html
 import uk.gov.hmrc.play.test.UnitSpec
 
-class SideBarTest extends UnitSpec with FakeIhtApp with HtmlSpec {
+class ArticleTest extends UnitSpec with FakeIhtApp with HtmlSpec {
 
-  "sidebar" must {
+  "article" must {
 
-    "display the correct value for sidebarLinks and sidebarClass" in {
-
-      val result = sidebar(Html("test"), Some("sidebar"))
+    "display the correct value for articleClasses, content and no lang attribute" in {
+      val result = article(Html("test"), false, None, false, "en")(messagesApi.preferred(createFakeRequest()))
       val doc = asDocument(result)
 
-      val tag = doc.getElementsByTag("aside")
-      tag.attr("class") shouldBe "sidebar"
+      val tag = doc.getElementsByTag("article")
+      tag.attr("articleClasses") shouldBe empty
       tag.attr("lang") shouldBe empty
       tag.html() shouldBe "test"
     }
 
     "display the lang attribute when currentLang is Welsh " in {
-      val result = sidebar(Html("test"), Some("sidebar"), "cy")
+      val result = article(Html("test"), false, None, false, "cy")(messagesApi.preferred(createFakeRequest()))
       val doc = asDocument(result)
 
-      val tag = doc.getElementsByAttribute("lang")
-      tag.attr("class") shouldBe "sidebar"
-      tag.attr("lang") should be ("cy")
+      val tag = doc.getElementsByTag("article")
+      tag.attr("articleClasses") shouldBe empty
+      tag.attr("lang") shouldBe "cy"
       tag.html() shouldBe "test"
     }
 
