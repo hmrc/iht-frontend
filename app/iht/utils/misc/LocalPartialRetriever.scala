6deb2da90b4228c3f31405c493baae16429f62eb
vineet.tyagi <vineet.tyagi@digital.hmrc.gov.uk>
2017-08-02 17:29:52 +0100

    IHT-4980 Add files

---
+++ app/iht/utils/misc/LocalPartialRetriever.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package iht.utils.misc
+
+import javax.inject.{Inject, Singleton}
+
+import iht.services.http.WsAllMethods
+import uk.gov.hmrc.play.partials.FormPartialRetriever
+import uk.gov.hmrc.play.frontend.filters.SessionCookieCryptoFilter
+
+/**
+  * Created by vineet on 02/08/17.
+  */
+@Singleton
+class LocalPartialRetriever @Inject() (override val httpGet: WsAllMethods) extends FormPartialRetriever {
+  override def crypto = SessionCookieCryptoFilter.encrypt
+}
