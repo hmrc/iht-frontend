8c56c40cec73658f973a5497b5ff3412c1b07802
vineet.tyagi <vineet.tyagi@digital.hmrc.gov.uk>
2017-08-11 14:07:56 +0100

    IHT-5061 Remove println

--- test/iht/controllers/application/assets/MoneyOwedControllerTest.scala
+++ test/iht/controllers/application/assets/MoneyOwedControllerTest.scala
@@ -65,8 +65,6 @@ class MoneyOwedControllerTest extends ApplicationControllerTest{
 
     "respond with OK on page load" in {
 
-      println()
-
       val applicationDetails = CommonBuilder.buildApplicationDetails
       val applicationDetailsTemp = CommonBuilder.buildApplicationDetails
 
