91597ffc0479edf854faa3f1ecaa15b5282e5c0f
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2017-10-09 09:51:02 +0100

    IHT-5139 WIP

--- app/iht/controllers/DeadlinesController.scala
+++ app/iht/controllers/DeadlinesController.scala
@@ -16,19 +16,38 @@
 
 package iht.controllers
 
+import iht.config.IhtFormPartialRetriever
+import iht.connector.CachingConnector
+import iht.views.html.{deadlines_application, deadlines_registration}
+import play.api.Play.current
+import play.api.i18n.Messages.Implicits._
 import uk.gov.hmrc.play.frontend.controller.{FrontendController, UnauthorisedAction}
+import uk.gov.hmrc.play.partials.FormPartialRetriever
+
+import scala.concurrent.Future
 
 /**
   * Created by yasar on 2/19/15.
   */
 
-trait DeadlinesController extends FrontendController{
-  def onPageLoad = UnauthorisedAction {
+trait DeadlinesController extends FrontendController {
+  implicit val formPartialRetriever: FormPartialRetriever = IhtFormPartialRetriever
+
+  def cachingConnector: CachingConnector
+
+  def onPageLoadRegistration = UnauthorisedAction.async {
     implicit request => {
-      //Ok(iht.views.html.deadlines(request, messages, ))
-      NotImplemented("Not implemented")
+      Future.successful(Ok(deadlines_registration.apply))
+    }
+  }
+
+  def onPageLoadApplication = UnauthorisedAction.async {
+    implicit request => {
+      Future.successful(Ok(deadlines_application.apply))
     }
   }
 }
 
-object DeadlinesController extends DeadlinesController
+object DeadlinesController extends DeadlinesController {
+  val cachingConnector = CachingConnector
+}
