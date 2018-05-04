abc79f25443dd4fac1b4aba8824660f0b7813e32
Scott Goodwin <scott.goodwin@digital.hmrc.gov.uk>
2018-05-04 08:09:10 +0100

    IHT content changes for use-service

---
+++ test/iht/testhelpers/UseService.scala
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package iht.testhelpers
+
+trait UseService {
+
+  val pageIHTFilterUseServiceBetween325000And1MillionParagraph0   = "If the gross value of the estate (ie the value before you subtract debts and mortgages) is between £325,000 and £1 million, you may have to use the IHT400 form to report the estate value."
+  val pageIHTFilterUseServiceBetween325000And1MillionIHTFormlink  = "IHT400 form"
+  val pageIHTFilterUseServiceBetween325000And1MillionSection4P1   = "You’ll be asked to log in to confirm who you are with HMRC."
+  val pageIHTFilterUseServiceBetween325000And1MillionReport       = "Continue to the online service"
+
+}
