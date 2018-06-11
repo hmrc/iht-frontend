e6baa7a7b7009a50cea6f03fa0fc23f991211235
abdi1994 <abdi1994@hotmail.com>
2018-06-11 14:59:35 +0100

    Changed internalServerErrorTemplate and additional tests

--- app/iht/controllers/application/declaration/DeclarationController.scala
+++ app/iht/controllers/application/declaration/DeclarationController.scala
@@ -38,7 +38,7 @@ import uk.gov.hmrc.play.partials.FormPartialRetriever
 import scala.concurrent.ExecutionContext.Implicits._
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
-import uk.gov.hmrc.http.{BadGatewayException, GatewayTimeoutException, HeaderCarrier}
+import uk.gov.hmrc.http.{BadGatewayException, GatewayTimeoutException, HeaderCarrier, Upstream5xxResponse}
 
 object DeclarationController extends DeclarationController with IhtConnectors {
   lazy val metrics: Metrics = Metrics
@@ -109,6 +109,12 @@ trait DeclarationController extends ApplicationController {
           } else {
             processApplicationOrRedirect
           }
+        } recover {
+          case ex: Upstream5xxResponse if ex.upstreamResponseCode == 502 &&
+            ex.message.contains("Service Unavailable") => {
+              Logger.warn("Service Unavailable while submitting application", ex)
+              InternalServerError(iht.views.html.estateReports.estateReports_error_serviceUnavailable())
+            }
         }
       }
   }
