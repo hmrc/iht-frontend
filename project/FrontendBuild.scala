ab37024d132fa2de765973d5423079a61ffa8ba5
Robert Webb <robert.webb@digital.hmrc.gov.uk>
2018-06-14 10:40:36 +0100

    updated frontend bootstrap

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -20,7 +20,7 @@ val compile = Seq(
   ws, cache,
   "uk.gov.hmrc" %% "url-builder" % "2.1.0",
   "uk.gov.hmrc" %% "http-caching-client" % httpCachingClientVersion,
-  "uk.gov.hmrc" %% "frontend-bootstrap" % "8.20.0",
+  "uk.gov.hmrc" %% "frontend-bootstrap" % "8.24.0",
   "uk.gov.hmrc" %% "play-partials" % "6.1.0",
   "uk.gov.hmrc" %% "domain" % "5.1.0",
   "uk.gov.hmrc" %% "play-language" % "3.4.0",
