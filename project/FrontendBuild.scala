7ab6abfe860144886461746e95079f58b73d46cc
AndrewRButcher <6122384+AndrewRButcher@users.noreply.github.com>
2019-07-09 08:44:56 +0100

    Dependencies Update

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -11,9 +11,9 @@ private object AppDependencies {
   import play.sbt.PlayImport._
   import play.core.PlayVersion
 
-  private val httpCachingClientVersion = "8.3.0"
+  private val httpCachingClientVersion = "8.4.0-play-26"
   private val jsonSchemaValidatorVersion = "2.2.6"
-  private val jsonVersion = "20180130"
+  private val jsonVersion = "20180813"
   private val wireMockVersion = "2.9.0"
 
   private val typesafe = "com.typesafe.play"
@@ -22,17 +22,17 @@ private object AppDependencies {
     ws, cache,
     "uk.gov.hmrc" %% "url-builder" % "3.1.0",
     "uk.gov.hmrc" %% "http-caching-client" % httpCachingClientVersion,
-    "uk.gov.hmrc" %% "bootstrap-play-26" % "0.39.0",
-    "uk.gov.hmrc" %% "auth-client" % "2.21.0-play-26",
+    "uk.gov.hmrc" %% "bootstrap-play-26" % "0.41.0",
+    "uk.gov.hmrc" %% "auth-client" % "2.22.0-play-26",
     "uk.gov.hmrc" %% "play-partials" % "6.9.0-play-26",
     "uk.gov.hmrc" %% "domain" % "5.6.0-play-26",
-    "uk.gov.hmrc" %% "govuk-template" % "5.30.0-play-26",
-    "uk.gov.hmrc" %% "play-ui" % "7.39.0-play-26",
+    "uk.gov.hmrc" %% "govuk-template" % "5.36.0-play-26",
+    "uk.gov.hmrc" %% "play-ui" % "7.40.0-play-26",
     "uk.gov.hmrc" %% "play-language" % "3.4.0",
     typesafe %% "play-json" % "2.6.13",
     typesafe %% "play-json-joda" % "2.6.13",
     "com.github.fge" % "json-schema-validator" % jsonSchemaValidatorVersion,
-    "org.apache.xmlgraphics" % "fop" % "2.1",
+    "org.apache.xmlgraphics" % "fop" % "2.3",
     "org.json" % "json" % jsonVersion
   )
 
@@ -45,13 +45,13 @@ private object AppDependencies {
     def apply() = new TestDependencies {
       override lazy val test = Seq(
         "uk.gov.hmrc" %% "http-caching-client" % httpCachingClientVersion % scope,
-        "uk.gov.hmrc" %% "hmrctest" % "3.8.0-play-26" % scope,
-        "org.scalatest" %% "scalatest" % "3.0.0" % scope,
+        "uk.gov.hmrc" %% "hmrctest" % "3.9.0-play-26" % scope,
+        "org.scalatest" %% "scalatest" % "3.0.8" % scope,
         "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2",
         "org.pegdown" % "pegdown" % "1.6.0" % scope,
-        "org.jsoup" % "jsoup" % "1.10.2" % scope,
+        "org.jsoup" % "jsoup" % "1.12.1" % scope,
         "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
-        "org.mockito" % "mockito-core" % "2.13.0" % scope,
+        "org.mockito" % "mockito-core" % "3.0.0" % scope,
         "com.github.fge" % "json-schema-validator" % jsonSchemaValidatorVersion % scope,
         "org.json" % "json" % jsonVersion % scope
       )
@@ -67,13 +67,13 @@ private object AppDependencies {
 
       override lazy val test = Seq(
         "uk.gov.hmrc" %% "http-caching-client" % httpCachingClientVersion % scope,
-        "uk.gov.hmrc" %% "hmrctest" % "3.0.0" % scope,
-        "org.scalatest" %% "scalatest" % "3.0.0" % scope,
+        "uk.gov.hmrc" %% "hmrctest" % "3.3.0" % scope,
+        "org.scalatest" %% "scalatest" % "3.0.8" % scope,
         "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2",
         "org.pegdown" % "pegdown" % "1.6.0" % scope,
-        "org.jsoup" % "jsoup" % "1.10.2" % scope,
+        "org.jsoup" % "jsoup" % "1.12.1" % scope,
         "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
-        "org.mockito" % "mockito-core" % "2.13.0" % scope,
+        "org.mockito" % "mockito-core" % "3.0.0" % scope,
         "com.github.fge" % "json-schema-validator" % jsonSchemaValidatorVersion % scope,
         "org.json" % "json" % jsonVersion % scope,
         "com.github.tomakehurst" %  "wiremock" % wireMockVersion % scope,
